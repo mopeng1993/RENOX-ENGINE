2f4647c5d3d647cfc5a049616abc7f1e2eb854eb
mopeng1993 <mopenggor93@gmail.com>
2026-05-20 23:27:58 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "RENOX ENGINE ONLINE");
+});
